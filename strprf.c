6f6b25fd409d35b686adeef4990e9e406ead544c
kartheepm <31505141+kartheepm@users.noreply.github.com>
2018-01-09 19:05:24 +0530

    Create strprf.c

---
+++ strprf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+string arr[200] = {"Vishal","Vidharba"};
+int n = sizeof(arr) / sizeof(arr[0]);
+string ans = commonPrefix(arr, n);
+if (ans.length())
+printf ("The longest common prefix is - %s",ans.c_str());
+else
+printf("There is no common prefix");
+}
