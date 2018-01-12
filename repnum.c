68510731baeee9bcd113d36c12fff7d20f043ac2
kartheepm <31505141+kartheepm@users.noreply.github.com>
2018-01-12 20:43:29 +0530

    Create repnum.c

---
+++ repnum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+  int i,arr[20],j,no;
+scanf("%d",&no);
+  for(i=0;i<no;i++)
+  {
+   scanf("%d",&arr[i]);
+  }
+  for(i=0; i<no; i++)
+   {
+    for(j=i+1;j<no;j++)
+    {
+    if(arr[i]==arr[j])
+    {
+    printf("%d\n",arr[i]);
+    }
+   }
+   }
+ }
