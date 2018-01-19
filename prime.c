3caf9cb2744cdfc4b338812d20fa962741032477
kartheepm <31505141+kartheepm@users.noreply.github.com>
2018-01-19 16:28:59 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+
+void main() {
+  int n, i, c = 0;
+ 
+  scanf("%d", &n);
+
+ {
+      if (n % i == 0) {
+         c++;
+      }
+  }
+
+  if (c == 2) {
+  printf("Prime");
+  }
+  else {
+  printf("not Prime");
+  }
+  
+}
