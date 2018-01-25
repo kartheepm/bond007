cdc30b03c3973eb688629ca29892c19c6af95bca
kartheepm <31505141+kartheepm@users.noreply.github.com>
2018-01-25 15:59:19 +0530

    Create assin.c

---
+++ assin.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+int a[100];
+int i, j,b,t;
+scanf("%d", &b);
+for (i = 0; i <b; i++)
+{
+scanf("%d", &a[i]);
+}
+for(i=0;i<b;i++)
+{
+for(j=i+1;j<b;j++)
+{
+if(a[i]>a[j])
+{
+t= a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+for (i=0;i<b;i++)
+{
+printf("%d\n",a[i]);
+}
+return 0;  
+}
