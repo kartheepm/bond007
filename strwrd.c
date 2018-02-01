53ff589fda29301f4d883cbf1e1aac688c646425
kartheepm <31505141+kartheepm@users.noreply.github.com>
2018-02-01 14:22:02 +0530

    Create strwrd.c

---
+++ strwrd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+char a[100];
+int l,i,b=1;
+gets(a);
+l=strlen(a);
+for(i=0;i<l;i++)
+{if(a[i]==' ')
+{b++;}
+}
+printf("%d",b);
+return 0;
+}
