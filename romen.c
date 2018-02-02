341ff109a2c70ccd41a0d88b84332621165ba53c
kartheepm <31505141+kartheepm@users.noreply.github.com>
2018-02-02 15:13:39 +0530

    Create romen.c

---
+++ romen.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char b[10],a[50]={I,II,III,IV,V,VI,VII,VIII,IX,X,XI,XII,XIII,XIV,XV,XVI,XVII,XVIII,XIX,XX};
+gets(b);
+j=strlen(b);
+for(i=0;i<=j;i++)
+{if(a[i]==b)
+    {printf("%d",i+1);}
+    
+return 0;
+}
+
