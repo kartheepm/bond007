ff94ce40a2f4b9c96ba3f9ccda53839c4a3ac594
kartheepm <31505141+kartheepm@users.noreply.github.com>
2018-02-02 20:29:31 +0530

    Update romen.c

--- romen.c
+++ romen.c
@@ -1,14 +1,36 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 int main()
 {
-char b[10],a[50]={I,II,III,IV,V,VI,VII,VIII,IX,X,XI,XII,XIII,XIV,XV,XVI,XVII,XVIII,XIX,XX};
-gets(b);
-j=strlen(b);
-for(i=0;i<=j;i++)
-{if(a[i]==b)
-    {printf("%d",i+1);}
-    
+char b[30];
+int a[30],l,i,j;
+scanf("%s",&b);
+l=strlen(b);
+for(i=0;i<l;i++)
+{
+switch (b[i])
+{
+case 'I':a[i]=1;
+break;
+case 'V':a[i]=5;
+ break;
+case 'X':a[i]=10;
+break;
+default :printf(" ");
+break;
+}
+}
+j=a[l-1];
+for(i=l-1;i>0;i--)
+{
+if(a[i]>a[i-1])
+{
+j=j-a[i-1];
+}
+if(a[i]<=a[i-1])
+{
+j=j+a[i-1];
+}}
+printf("%d",j);
 return 0;
 }
-
