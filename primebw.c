5821c04f5631345382bce4bae101d469c3e57116
kartheepm <31505141+kartheepm@users.noreply.github.com>
2018-02-09 15:12:52 +0530

    Create primebw.c

---
+++ primebw.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int m,n,i,f=0;
+    scanf("%d",&m);
+scanf("%d",&n);
+    for(i=m+1;i<n;++i)
+    {
+        if(%i==0)
+        {
+            f=1;
+            break;
+        }
+    }
+
+    if (f==0)
+        printf("%d,n);
+    else
+        printf("%d",n);
+    
+    return 0;
+}
